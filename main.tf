c77da61052eaed1d0dc5375bf9cc5f05c2c5b361
godwin-pk <80232410+godwin-pk@users.noreply.github.com>
2022-02-15 22:23:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,4 +12,9 @@ module "aft" {
   account_provisioning_customizations_repo_name = "${var.github_username}/learn-terraform-aft-account-provisioning-customizations"
   global_customizations_repo_name               = "${var.github_username}/learn-terraform-aft-global-customizations"
   account_customizations_repo_name              = "${var.github_username}/learn-terraform-aft-account-customizations"
+  
+  # TF Vars
+  terraform_distribution = "tfc"
+  terraform_token        = "EXAMPLE-uoc1c1qsw7poexampleewjeno1pte3rw"
+  terraform_org_name     = "ExampleOrg" 
 }
